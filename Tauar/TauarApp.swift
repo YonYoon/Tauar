9b849f506b86ac7e8e07ce5e624a49b54b96ac50
YonYoon <zhzhzh1806@gmail.com>
2024-06-07 14:09:54 +0500

    Initial Commit

---
+++ Tauar/TauarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TauarApp.swift
+//  Tauar
+//
+//  Created by Zhansen Zhalel on 07.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TauarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
